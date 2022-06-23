c615fb62b76dab566f6292addfb69578285694bf
lucasvs07 <78516282+lucasvs07@users.noreply.github.com>
2022-06-23 14:25:29 -0300

    Implementado IRepositório no módulo compartilhado

---
+++ LocadoraDeVeiculos.Dominio/Compartilhado/Irepositorio.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation.Results;
+using System.Collections.Generic;
+
+
+namespace LocadoraDeVeiculos.Dominio.Compartilhado
+{
+    public interface Irepositorio <T> where T : EntidadeBase<T>
+    {
+        ValidationResult Inserir(T novoRegistro);
+
+        ValidationResult Editar(T registro);
+
+        ValidationResult Excluir(T registro);
+
+        List<T> SelecionarTodos();
+
+        T SelecionarPorId(int id);
+    }
+}
