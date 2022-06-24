5a9a8f51a7a5772566ec61eab1ce8c6102fd5b80
eveliseoliveira <eveliseoliveira.s@gmail.com>
2022-06-24 16:02:26 -0300

    teste

--- LocadoraDeVeiculos.Infra/ModuloGrupoVeiculos/RepositorioGrupoVeiculosEmBancoDados.cs
+++ LocadoraDeVeiculos.Infra/ModuloGrupoVeiculos/RepositorioGrupoVeiculosEmBancoDados.cs
@@ -1,4 +1,5 @@
-﻿using LocadoraDeVeiculos.Dominio.ModuloGrupoVeiculos;
+﻿using FluentValidation.Results;
+using LocadoraDeVeiculos.Dominio.ModuloGrupoVeiculos;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,29 @@ namespace LocadoraDeVeiculos.Infra.ModuloGrupoVeiculos
 {
     public class RepositorioGrupoVeiculosEmBancoDados : IRepositorioGrupoVeiculos
     {
+        public ValidationResult Editar(GrupoDeVeiculos registro)
+        {
+            throw new NotImplementedException();
+        }
 
+        public ValidationResult Excluir(GrupoDeVeiculos registro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValidationResult Inserir(GrupoDeVeiculos novoRegistro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public GrupoDeVeiculos SelecionarPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<GrupoDeVeiculos> SelecionarTodos()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
