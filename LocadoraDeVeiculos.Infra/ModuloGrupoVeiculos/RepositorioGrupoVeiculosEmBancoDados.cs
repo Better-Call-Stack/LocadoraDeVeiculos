1717dfcbadc5cb8962d73765582cc558c7eb1a7f
eveliseoliveira <eveliseoliveira.s@gmail.com>
2022-06-24 14:16:21 -0300

    Grid Grupo de Vaiculos

---
+++ LocadoraDeVeiculos.Infra/ModuloGrupoVeiculos/RepositorioGrupoVeiculosEmBancoDados.cs
@@ -0,0 +1,14 @@
+﻿using LocadoraDeVeiculos.Dominio.ModuloGrupoVeiculos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Infra.ModuloGrupoVeiculos
+{
+    public class RepositorioGrupoVeiculosEmBancoDados : IRepositorioGrupoVeiculos
+    {
+
+    }
+}
--- LocadoraDeVeiculos.WinApp/ModuloGrupoVeiculos/TabelaGrupoVeiculosControl.cs
+++ LocadoraDeVeiculos.WinApp/ModuloGrupoVeiculos/TabelaGrupoVeiculosControl.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LocadoraDeVeiculos.Dominio.ModuloGrupoVeiculos;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,23 @@ namespace LocadoraDeVeiculos.WinApp.ModuloGrupoVeiculos
         public TabelaGrupoVeiculosControl()
         {
             InitializeComponent();
+
+        }
+
+        internal void AtualizarRegistro(List<GrupoDeVeiculos> grupoVeiculos)
+        {
+            gridGrupoVeiculos.Rows.Clear();
+
+            foreach (GrupoDeVeiculos grupoDeVeiculo in grupoVeiculos)
+            {
+                gridGrupoVeiculos.Rows.Add(grupoDeVeiculo.Id, grupoDeVeiculo.Nome, grupoDeVeiculo.ValorPlanoDiario, 
+                    grupoDeVeiculo.ValorDiariaKmControlado, grupoDeVeiculo.ValorDiarioKmLivre);
+            }
+        }
+
+        internal GrupoDeVeiculos SelecionarGrupoVeiculos()
+        {
+            return new GrupoDeVeiculos();
         }
     }
 }
