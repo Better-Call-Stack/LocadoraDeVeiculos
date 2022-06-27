d662613a9a4a9abcef4fd02f6d867da05b2dc588
Joao Pedro T. Correa <joaopedrotcorrea1212@gmail.com>
2022-06-27 00:57:09 -0300

    Testes Taxa. Inserir, Edição e Exclusao

---
+++ LocadoraDeVeiculos.Infra.Tests/ModuloTaxa/RepositorioTaxaTests.cs
@@ -0,0 +1,79 @@
+﻿using FluentAssertions;
+using LocadoraDeVeiculos.Dominio.ModuloTaxa;
+using LocadoraDeVeiculos.Infra.Compartilhado;
+using LocadoraDeVeiculos.Infra.ModuloTaxa;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Infra.Tests.ModuloTaxa
+{
+    public class RepositorioTaxaTests
+    {
+        [TestClass]
+        public class RepositorioClienteTests
+        {
+            Taxa taxa;
+            RepositorioTaxa repositorio = new RepositorioTaxa();
+
+            public RepositorioClienteTests()
+            {
+                Db.ExecutarSql("DELETE FROM TBCLIENTE; DBCC CHECKIDENT (TBCLIENTE, RESEED, 0)");
+
+                taxa = new Taxa();
+                taxa.Nome = "Cadeira Bebe";
+                taxa.Valor = (decimal)25.5;
+                taxa.Tipo = TipoCalculoTaxa.Diario;
+
+
+            }
+
+            [TestMethod]
+            public void Deve_Inserir_Cliente()
+            {
+                repositorio.Inserir(taxa);
+
+                Taxa t = repositorio.SelecionarPorId(1);
+
+                t.Should().NotBeNull().And.Be(taxa);
+
+            }
+
+            [TestMethod]
+            public void Deve_Editar_Cliente()
+            {
+                repositorio.Inserir(taxa);
+
+                Taxa taxaAtualizada = repositorio.SelecionarPorId(taxa.Id);
+                taxaAtualizada.Nome = "Lavação";
+                taxaAtualizada.Tipo = TipoCalculoTaxa.Fixo;
+                taxaAtualizada.Valor = (decimal)30.0;
+
+                //action
+                repositorio.Editar(taxaAtualizada);
+
+                //assert
+                Taxa t = repositorio.SelecionarPorId(taxa.Id);
+
+                t.Should().NotBeNull();
+                t.Nome.Should().Be("Lavação");
+                t.Valor.Should().Be(30);
+                t.Tipo.Should().Be(TipoCalculoTaxa.Fixo);
+            }
+
+            [TestMethod]
+            public void Deve_Excluir_Cliente()
+            {
+                repositorio.Inserir(taxa);
+
+                repositorio.Excluir(taxa);
+
+                repositorio.SelecionarTodos().Count().Should().Be(0);
+            }
+        }
+    }
+}
+
