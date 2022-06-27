169a10a445e578fbffcf0d5a31c9e3d63bab6962
Joao Pedro T. Correa <joaopedrotcorrea1212@gmail.com>
2022-06-26 23:34:24 -0300

    Banco de Dados

---
+++ LocadoraDeVeiculos.Projeto.BancoDeDados/TBCliente.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[TbCliente] (
+    [Id]         INT          IDENTITY (1, 1) NOT NULL,
+    [Nome]       VARCHAR (50) NOT NULL,
+    [CPF]        VARCHAR (50) NULL,
+    [CNPJ]       VARCHAR (50) NULL,
+    [Cidade]     VARCHAR (50) NOT NULL,
+    [Endereco]   VARCHAR (50) NOT NULL,
+    [Telefone]   VARCHAR (50) NOT NULL,
+    [CNH]        VARCHAR (50) NOT NULL,
+    [Email]      VARCHAR (50) NULL,
+    [TipoPessoa] INT          NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
