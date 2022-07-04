3deca52a480dd5ab2cd8b6eb0aa1b961aa6bc83d
lucasvs07 <78516282+lucasvs07@users.noreply.github.com>
2022-07-04 16:30:40 -0300

    Criada tabela de veículo no projeto SQL

---
+++ LocadoraDeVeiculos.Projeto.BancoDeDados/TBVeiculo.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[TbVeiculo] (
+    [id]                 INT            IDENTITY (1, 1) NOT NULL,
+    [Modelo]             VARCHAR (50)   NOT NULL,
+    [Fabricante]         VARCHAR (50)   NOT NULL,
+    [Placa]              VARCHAR (7)    NOT NULL,
+    [Cor]                VARCHAR (50)   NOT NULL,
+    [TipoCombustivel]    INT            NOT NULL,
+    [CapacidadeDoTanque] DECIMAL (5, 2) NOT NULL,
+    [Ano]                INT            NOT NULL,
+    [KmPercorrido]       INT            NULL,
+    [GrupoVeiculos_Id]   INT            NOT NULL,
+    CONSTRAINT [PK_TbVeiculo] PRIMARY KEY CLUSTERED ([id] ASC),
+    CONSTRAINT [FK_TbVeiculo_TBGrupoVeiculos] FOREIGN KEY ([GrupoVeiculos_Id]) REFERENCES [dbo].[TBGrupoVeiculos] ([Id])
+);
