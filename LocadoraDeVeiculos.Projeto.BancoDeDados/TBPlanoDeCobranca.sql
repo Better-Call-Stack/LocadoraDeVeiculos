cf2d3ffb2c578b2902e0955852fcc3578260a677
eveliseoliveira <eveliseoliveira.s@gmail.com>
2022-07-08 14:44:06 -0300

    Correção Tabela

--- LocadoraDeVeiculos.Projeto.BancoDeDados/TBPlanoDeCobranca.sql
+++ LocadoraDeVeiculos.Projeto.BancoDeDados/TBPlanoDeCobranca.sql
@@ -1,10 +1,12 @@
-﻿CREATE TABLE [dbo].[TBPlanoDeCobranca]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [txtValorKmRodado_PlanoDiario] DECIMAL(10, 2) NOT NULL, 
-    [txtValorPorDia_PlanoDiario] DECIMAL(10, 2) NOT NULL, 
-    [txtValorKmRodado_PlanoKmControlado] DECIMAL(10, 2) NOT NULL, 
-    [txtKmLivreIncluso_PlanoKmControlado] DECIMAL(10, 2) NOT NULL, 
-    [txtValorPorDia_PlanoKmControlado] DECIMAL(10, 2) NOT NULL, 
-    [txtValorPorDia_PlanoKmLivre] DECIMAL(10, 2) NOT NULL
-)
+﻿CREATE TABLE [dbo].[TBPlanoDeCobranca] (
+    [Id]                                  INT             IDENTITY (1, 1) NOT NULL,
+    [txtValorKmRodado_PlanoDiario]        DECIMAL (10, 2) NOT NULL,
+    [txtValorPorDia_PlanoDiario]          DECIMAL (10, 2) NOT NULL,
+    [txtValorKmRodado_PlanoKmControlado]  DECIMAL (10, 2) NOT NULL,
+    [txtKmLivreIncluso_PlanoKmControlado] DECIMAL (10, 2) NOT NULL,
+    [txtValorPorDia_PlanoKmControlado]    DECIMAL (10, 2) NOT NULL,
+    [txtValorPorDia_PlanoKmLivre]         DECIMAL (10, 2) NOT NULL,
+    [GrupoVeiculos_Id]                    INT             NOT NULL,
+    CONSTRAINT [PK_TBPlanoDeCobranca] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_TBPlanoDeCobranca_TBGrupoVeiculos] FOREIGN KEY ([GrupoVeiculos_Id]) REFERENCES [dbo].[TBGrupoVeiculos] ([Id])
+);
