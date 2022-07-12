981838d47d38d38beee87ccd92ba6cb6b9af8ba5
eveliseoliveira <eveliseoliveira.s@gmail.com>
2022-07-12 18:33:50 -0300

    Refatoração logs no Plano de Cobrança, correção erro banco de dados editar Plano de Cobrança

--- LocadoraDeVeiculos.WinApp/Program.cs
+++ LocadoraDeVeiculos.WinApp/Program.cs
@@ -1,4 +1,5 @@
 using LocadoraVeiculos.Infra.Logging;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +16,12 @@ namespace LocadoraDeVeiculos.WinApp
         [STAThread]
         static void Main()
         {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.File("logs/log.txt", 
+                         rollingInterval: RollingInterval.Day,
+                         outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
+                        .CreateLogger();
+
             ConfiguracaoLogsLocadora.ConfigurarEscritaLogs();
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
