635a64e6972ce20f03cbf408eb1d5f244a354648
Joao Pedro T. Correa <joaopedrotcorrea1212@gmail.com>
2022-07-13 17:22:42 -0300

    log Funcionario

--- LocadoraVeiculos.Aplicacao/ModuloFuncionario/ServicoFuncionario.cs
+++ LocadoraVeiculos.Aplicacao/ModuloFuncionario/ServicoFuncionario.cs
@@ -22,11 +22,24 @@ namespace LocadoraVeiculos.Aplicacao.ModuloFuncionario
 
         public ValidationResult Inserir(Funcionario funcionario)
         {
+            Log.Logger.Debug("Tentando inserir funcionario... {@f}", funcionario);
+
             var resultadoValidacao = Validar(funcionario);
 
             if (resultadoValidacao.IsValid)
+            {
                 repositorioFuncionario.Inserir(funcionario);
+                Log.Logger.Debug("Funcionario {FuncionarioNome} inserido com sucesso", funcionario.Nome);
+            }
+            else
+            {
+                foreach (var erro in resultadoValidacao.Errors)
+                {
+                    Log.Logger.Warning("Falha ao tentar inserir um Funcionario {FuncionarioNome} - {Motivo}",
+                        funcionario.Nome, erro.ErrorMessage);
+                }
 
+            }
             return resultadoValidacao;
         }
 
@@ -75,5 +88,5 @@ namespace LocadoraVeiculos.Aplicacao.ModuloFuncionario
             return resultadoValidacao;
         }
     }
-    }
+   
 }
