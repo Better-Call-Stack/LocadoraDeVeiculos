187f441014868c6043067b705fa152c43940d667
eveliseoliveira <eveliseoliveira.s@gmail.com>
2022-07-29 13:16:12 -0300

    Implementado Tela e Configuraçãoo Toolbox do Cadastro de Devolução

---
+++ LocadoraDeVeiculos.WinApp/ModuloDevolucao/ConfiguracaoToolboxDevolucao.cs
@@ -0,0 +1,20 @@
+﻿using LocadoraDeVeiculos.WinApp.Compartilhado;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.WinApp.ModuloDevolucao
+{
+    public class ConfiguracaoToolboxDevolucao : ConfiguracaoToolboxBase
+    {
+        public override string TipoCadastro => "Controle de Devoluções";
+
+        public override string TooltipInserir => "Inserir uma nova devolução";
+
+        public override string TooltipEditar => "Editar uma devolução";
+
+        public override string TooltipExcluir => "Excluir uma devolução";
+    }
+}
---
+++ LocadoraDeVeiculos.WinApp/ModuloDevolucao/TelaCadastroDevolucao.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace LocadoraDeVeiculos.WinApp.ModuloDevolucao
+{
+    partial class TelaCadastroDevolucao
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCliente = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtVeiculo = new System.Windows.Forms.TextBox();
+            this.txtQuilometragem = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtValorGasolina = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBoxVolumeTanque = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.listTaxasAdicionais = new System.Windows.Forms.ListBox();
+            this.sqlCommandBuilder1 = new Microsoft.Data.SqlClient.SqlCommandBuilder();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnVisualizar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtCliente
+            // 
+            this.txtCliente.Location = new System.Drawing.Point(42, 62);
+            this.txtCliente.Name = "txtCliente";
+            this.txtCliente.Size = new System.Drawing.Size(223, 27);
+            this.txtCliente.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Cliente:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Veículo:";
+            // 
+            // txtVeiculo
+            // 
+            this.txtVeiculo.Location = new System.Drawing.Point(42, 126);
+            this.txtVeiculo.Name = "txtVeiculo";
+            this.txtVeiculo.Size = new System.Drawing.Size(223, 27);
+            this.txtVeiculo.TabIndex = 3;
+            // 
+            // txtQuilometragem
+            // 
+            this.txtQuilometragem.Location = new System.Drawing.Point(338, 62);
+            this.txtQuilometragem.Name = "txtQuilometragem";
+            this.txtQuilometragem.Size = new System.Drawing.Size(223, 27);
+            this.txtQuilometragem.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(338, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Quilometragem:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(338, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(141, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Data de Dovolução:";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(338, 126);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(223, 27);
+            this.dateTimePicker1.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(338, 167);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(128, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Valor da Gasolina:";
+            // 
+            // txtValorGasolina
+            // 
+            this.txtValorGasolina.Location = new System.Drawing.Point(338, 191);
+            this.txtValorGasolina.Name = "txtValorGasolina";
+            this.txtValorGasolina.Size = new System.Drawing.Size(223, 27);
+            this.txtValorGasolina.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(42, 167);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(135, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Volume do Tanque:";
+            // 
+            // comboBoxVolumeTanque
+            // 
+            this.comboBoxVolumeTanque.FormattingEnabled = true;
+            this.comboBoxVolumeTanque.Location = new System.Drawing.Point(42, 190);
+            this.comboBoxVolumeTanque.Name = "comboBoxVolumeTanque";
+            this.comboBoxVolumeTanque.Size = new System.Drawing.Size(223, 28);
+            this.comboBoxVolumeTanque.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(42, 233);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(120, 20);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Taxas Adicionais:";
+            // 
+            // listTaxasAdicionais
+            // 
+            this.listTaxasAdicionais.FormattingEnabled = true;
+            this.listTaxasAdicionais.ItemHeight = 20;
+            this.listTaxasAdicionais.Location = new System.Drawing.Point(42, 256);
+            this.listTaxasAdicionais.Name = "listTaxasAdicionais";
+            this.listTaxasAdicionais.Size = new System.Drawing.Size(519, 124);
+            this.listTaxasAdicionais.TabIndex = 14;
+            // 
+            // sqlCommandBuilder1
+            // 
+            this.sqlCommandBuilder1.DataAdapter = null;
+            this.sqlCommandBuilder1.QuotePrefix = "[";
+            this.sqlCommandBuilder1.QuoteSuffix = "]";
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancelar.Location = new System.Drawing.Point(467, 406);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(94, 29);
+            this.btnCancelar.TabIndex = 15;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnVisualizar
+            // 
+            this.btnVisualizar.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnVisualizar.Location = new System.Drawing.Point(361, 406);
+            this.btnVisualizar.Name = "btnVisualizar";
+            this.btnVisualizar.Size = new System.Drawing.Size(94, 29);
+            this.btnVisualizar.TabIndex = 16;
+            this.btnVisualizar.Text = "Visualizar";
+            this.btnVisualizar.UseVisualStyleBackColor = true;
+            // 
+            // TelaCadastroDevolucao
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(605, 464);
+            this.Controls.Add(this.btnVisualizar);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.listTaxasAdicionais);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.comboBoxVolumeTanque);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtValorGasolina);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtQuilometragem);
+            this.Controls.Add(this.txtVeiculo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCliente);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "TelaCadastroDevolucao";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cadastro de Devolução";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCliente;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtVeiculo;
+        private System.Windows.Forms.TextBox txtQuilometragem;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtValorGasolina;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBoxVolumeTanque;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ListBox listTaxasAdicionais;
+        private Microsoft.Data.SqlClient.SqlCommandBuilder sqlCommandBuilder1;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnVisualizar;
+    }
+}
