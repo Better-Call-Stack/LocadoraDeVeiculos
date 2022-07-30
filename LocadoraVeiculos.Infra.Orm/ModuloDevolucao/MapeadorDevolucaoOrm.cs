363059e66785bcfae0b91227d5c77c09f09a4bb9
eveliseoliveira <eveliseoliveira.s@gmail.com>
2022-07-29 23:03:45 -0300

    Implementado Repositorio Devolucao Orm e  Mapeador Devolucao Orm

---
+++ LocadoraVeiculos.Infra.Orm/ModuloDevolucao/MapeadorDevolucaoOrm.cs
@@ -0,0 +1,26 @@
+﻿using LocadoraDeVeiculos.Dominio.ModuloDevolucao;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Infra.Orm.ModuloDevolucao
+{
+    public class MapeadorDevolucaoOrm : IEntityTypeConfiguration<Devolucao>
+    {
+        public void Configure(EntityTypeBuilder<Devolucao> builder)
+        {
+            builder.ToTable("TBDevolucao");
+            builder.Property(x => x.Id).ValueGeneratedNever();
+            builder.Property(x => x.ValorGasolina).HasColumnType("decimal(10,2)");
+            builder.Property(x => x.Quilometragem).HasColumnType("decimal(18,2)");
+            builder.Property(x => x.DataDevolucao).HasColumnType("date");
+            builder.Property(x => x.VolumeTanque).HasColumnType("int");
+            builder.HasOne(x => x.Locacao);
+            builder.HasOne(x => x.Taxa);
+        }
+    }
+}
---
+++ LocadoraVeiculos.Infra.Orm/ModuloDevolucao/RepositorioDevolucaoOrm.cs
@@ -0,0 +1,53 @@
+﻿using LocadoraDeVeiculos.Dominio.ModuloDevolucao;
+using LocadoraVeiculos.Infra.Orm;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Infra.Orm.ModuloDevolucao
+{
+    public class RepositorioDevolucaoOrm : IRepositorioDevolucao
+    {
+        private DbSet<Devolucao> devolucao;
+        private readonly LocadoraDeVeiculosDbContext dbContext;
+        public RepositorioDevolucaoOrm(LocadoraDeVeiculosDbContext dbContext)
+        {
+            devolucao = dbContext.Set<Devolucao>();
+            this.dbContext = dbContext;
+        }
+        public void Inserir(Devolucao novoRegistro)
+        {
+            devolucao.Add(novoRegistro);
+        }
+
+        public void Editar(Devolucao registro)
+        {
+            devolucao.Update(registro);
+        }
+
+        public void Excluir(Devolucao registro)
+        {
+            devolucao.Remove(registro);
+        }
+
+        public Devolucao SelecionarPorId(Guid id)
+        {
+            return devolucao.SingleOrDefault(d => d.Id == id);
+        }
+        public List<Devolucao> SelecionarTodos(bool incluirLocacao = false)
+        {
+            if (incluirLocacao)
+                return devolucao.Include(d => d.Locacao).ToList();
+
+            return devolucao.ToList();
+        }
+
+        public List<Devolucao> SelecionarTodos()
+        {
+            return devolucao.ToList();
+        }
+    }
+}
