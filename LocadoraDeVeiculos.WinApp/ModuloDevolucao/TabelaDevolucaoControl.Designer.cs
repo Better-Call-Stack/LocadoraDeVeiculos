bea29324b9800d218eef717bee6b43fbbf4da1d0
eveliseoliveira <eveliseoliveira.s@gmail.com>
2022-07-30 16:12:36 -0300

    Implementado TabelaDevolucaoControl e TabelaDevolucao

---
+++ LocadoraDeVeiculos.WinApp/ModuloDevolucao/TabelaDevolucaoControl.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace LocadoraDeVeiculos.WinApp.ModuloDevolucao
+{
+    partial class TabelaDevolucaoControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabelaControl = new System.Windows.Forms.TabControl();
+            this.tabAtivos = new System.Windows.Forms.TabPage();
+            this.gridAtivos = new System.Windows.Forms.DataGridView();
+            this.tabInativo = new System.Windows.Forms.TabPage();
+            this.gridInativo = new System.Windows.Forms.DataGridView();
+            this.tabelaControl.SuspendLayout();
+            this.tabAtivos.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridAtivos)).BeginInit();
+            this.tabInativo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridInativo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabelaControl
+            // 
+            this.tabelaControl.Controls.Add(this.tabAtivos);
+            this.tabelaControl.Controls.Add(this.tabInativo);
+            this.tabelaControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabelaControl.Location = new System.Drawing.Point(0, 0);
+            this.tabelaControl.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tabelaControl.Name = "tabelaControl";
+            this.tabelaControl.SelectedIndex = 0;
+            this.tabelaControl.Size = new System.Drawing.Size(710, 545);
+            this.tabelaControl.TabIndex = 1;
+            // 
+            // tabAtivos
+            // 
+            this.tabAtivos.Controls.Add(this.gridAtivos);
+            this.tabAtivos.Location = new System.Drawing.Point(4, 29);
+            this.tabAtivos.Name = "tabAtivos";
+            this.tabAtivos.Padding = new System.Windows.Forms.Padding(3);
+            this.tabAtivos.Size = new System.Drawing.Size(702, 512);
+            this.tabAtivos.TabIndex = 0;
+            this.tabAtivos.Text = "Ativos";
+            this.tabAtivos.UseVisualStyleBackColor = true;
+            // 
+            // gridAtivos
+            // 
+            this.gridAtivos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridAtivos.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridAtivos.Location = new System.Drawing.Point(3, 3);
+            this.gridAtivos.Name = "gridAtivos";
+            this.gridAtivos.RowHeadersWidth = 51;
+            this.gridAtivos.RowTemplate.Height = 29;
+            this.gridAtivos.Size = new System.Drawing.Size(696, 506);
+            this.gridAtivos.TabIndex = 0;
+            // 
+            // tabInativo
+            // 
+            this.tabInativo.Controls.Add(this.gridInativo);
+            this.tabInativo.Location = new System.Drawing.Point(4, 29);
+            this.tabInativo.Name = "tabInativo";
+            this.tabInativo.Padding = new System.Windows.Forms.Padding(3);
+            this.tabInativo.Size = new System.Drawing.Size(702, 512);
+            this.tabInativo.TabIndex = 1;
+            this.tabInativo.Text = "Inativos";
+            this.tabInativo.UseVisualStyleBackColor = true;
+            // 
+            // gridInativo
+            // 
+            this.gridInativo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridInativo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridInativo.Location = new System.Drawing.Point(3, 3);
+            this.gridInativo.Name = "gridInativo";
+            this.gridInativo.RowHeadersWidth = 51;
+            this.gridInativo.RowTemplate.Height = 29;
+            this.gridInativo.Size = new System.Drawing.Size(696, 506);
+            this.gridInativo.TabIndex = 0;
+            // 
+            // TabelaDevolucaoControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tabelaControl);
+            this.Name = "TabelaDevolucaoControl";
+            this.Size = new System.Drawing.Size(710, 545);
+            this.tabelaControl.ResumeLayout(false);
+            this.tabAtivos.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gridAtivos)).EndInit();
+            this.tabInativo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gridInativo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabelaControl;
+        private System.Windows.Forms.TabPage tabAtivos;
+        private System.Windows.Forms.TabPage tabInativo;
+        private System.Windows.Forms.DataGridView gridAtivos;
+        private System.Windows.Forms.DataGridView gridInativo;
+    }
+}
---
+++ LocadoraDeVeiculos.WinApp/ModuloDevolucao/TabelaDevolucaoControl.cs
@@ -0,0 +1,100 @@
+﻿using LocadoraDeVeiculos.Dominio.ModuloDevolucao;
+using LocadoraDeVeiculos.Dominio.ModuloLocacao;
+using LocadoraDeVeiculos.WinApp.Compartilhado;
+using LocadoraVeiculos.Aplicacao.ModuloDevolucao;
+using LocadoraVeiculos.Aplicacao.ModuloLocacao;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LocadoraDeVeiculos.WinApp.ModuloDevolucao
+{
+    public partial class TabelaDevolucaoControl : UserControl
+    {
+
+
+        public TabelaDevolucaoControl()
+        {
+            InitializeComponent();
+            gridAtivos.ConfigurarGridZebrado();
+            gridAtivos.ConfigurarGridSomenteLeitura();
+            gridInativo.ConfigurarGridZebrado();
+            gridInativo.ConfigurarGridSomenteLeitura();
+            gridAtivos.Columns.AddRange(ObterColunasAtivos());
+            gridInativo.Columns.AddRange(ObterColunasInativo());
+        }
+
+        public DataGridViewColumn[] ObterColunasAtivos()
+        {
+            var colunas = new DataGridViewColumn[]
+            {
+                new DataGridViewTextBoxColumn { DataPropertyName = "Id", HeaderText = "Id", FillWeight=15F, Visible=false },
+
+                new DataGridViewTextBoxColumn { DataPropertyName = "Cliente", HeaderText = "Cliente"},
+
+                new DataGridViewTextBoxColumn { DataPropertyName = "Condutor", HeaderText = "Condutor"},
+
+                new DataGridViewTextBoxColumn { DataPropertyName = "Veiculo", HeaderText = "Veiculo"},
+
+                new DataGridViewTextBoxColumn { DataPropertyName = "Plano", HeaderText = "Plano"},
+            };
+
+            return colunas;
+        }
+        public DataGridViewColumn[] ObterColunasInativo()
+        {
+            var colunas = new DataGridViewColumn[]
+            {
+                new DataGridViewTextBoxColumn { DataPropertyName = "Id", HeaderText = "Id", FillWeight=15F, Visible=false },
+
+                new DataGridViewTextBoxColumn { DataPropertyName = "Cliente", HeaderText = "Cliente"},
+
+                new DataGridViewTextBoxColumn { DataPropertyName = "Condutor", HeaderText = "Condutor"},
+
+                new DataGridViewTextBoxColumn { DataPropertyName = "Veiculo", HeaderText = "Veiculo"},
+
+                new DataGridViewTextBoxColumn { DataPropertyName = "DataDevolucao", HeaderText = "Devolução"},
+            };
+
+            return colunas;
+        }
+
+        public Guid ObtemIdSelecionado()
+        {
+            if (tabelaControl.SelectedTab.Name == "tabAtivos")
+            {
+                return gridAtivos.SelecionarId<Guid>();
+            }
+            else
+            {
+                return gridInativo.SelecionarId<Guid>();
+            }
+        }
+
+        public void AtualizarRegistros(List<Locacao> locacoes, List<Devolucao> devolucoes)
+        {
+            gridAtivos.Rows.Clear();
+            gridInativo.Rows.Clear();
+
+            foreach (var locacao in locacoes)
+            {
+                gridAtivos.Rows.Add(locacao.Id, locacao.Cliente.Nome, locacao.Condutor.Nome, 
+                    locacao.Veiculo.Modelo, locacao.PlanoSelecionado);
+            }
+
+            foreach (Devolucao devolucao in devolucoes)
+            {
+                gridInativo.Rows.Add(devolucao.Id, devolucao.Locacao.Cliente.Nome,
+                    devolucao.Locacao.Condutor.Nome, devolucao.Locacao.Veiculo.Modelo, devolucao.DataDevolucao);
+            }
+        }
+
+
+    }
+}
