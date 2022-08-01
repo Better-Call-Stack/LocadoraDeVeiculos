147a7143dabad8184aded02afce15532e129a328
Joao Pedro T. Correa <joaopedrotcorrea1212@gmail.com>
2022-08-01 00:13:01 -0300

    .

---
+++ LocadoraVeiculos.Infra.Orm/Migrations/20220801031253_TbLocacaoValores.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LocadoraDeVeiculos.Infra.Orm.Migrations
+{
+    public partial class TbLocacaoValores : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Valor",
+                table: "TbLocacao",
+                newName: "ValorDiario");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Subtotal",
+                table: "TbLocacao",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Subtotal",
+                table: "TbLocacao");
+
+            migrationBuilder.RenameColumn(
+                name: "ValorDiario",
+                table: "TbLocacao",
+                newName: "Valor");
+        }
+    }
+}
